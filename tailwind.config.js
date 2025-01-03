18a51e096e8ebb57e72081f478c0e0fd89323166
Sherry <phamthuthu204@gmail.com>
2025-01-03 11:58:43 +1100

    set up fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,40 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik-Regular', 'san-serif'],
+        "rubik-bold":["Rubik-Bold", "san-serif"],
+        "rubik-extrabold":["Rubik-ExtraBold", "san-serif"],
+        "rubik-medium":["Rubik-Medium", "san-serif"],
+        "rubik-semibold":["Rubik-SemiBold", "san-serif"],
+        "rubik-light":["Rubik-Light", "san-serif"],
+      },
+      colors: {
+        // "primary": "#FF6A00",
+        // "secondary": "#FFA400",
+        // "tertiary": "#FFD700",
+        // "dark": "#1A1A1A",
+        // "light": "#F5F5F5",
+        // "white": "#FFFFFF",
+        // "black": "#000000",
+        "primary": {
+          100: '#0061FF0A',
+          200: '#0061FF1A',
+          300: '#0061FF',
+        },
+        accent: {
+          100: '#FBFBFD',
+        },
+        black: {
+          DEFAULT: '#000000',
+          100: '#8C8E98',
+          200: '#666876',
+          300: '#191d31',
+        },
+        danger: '#F75555',
+      }
+    },
   },
   plugins: [],
 }
